b9c9a67d0cc5283a557deedddfd83979fbecaab5
artemowork <artem180791@mail.ru>
2024-04-21 22:01:58 +0300

    добавление констант

---
+++ src/main/java/org/example/constance/ConstanceApplication.java
@@ -0,0 +1,9 @@
+package org.example.constance;
+
+public class ConstanceApplication {
+    /**
+     * Constance limit_credit
+     */
+    public final static Integer LIMIT_CREDIT_SIMPLE = 10000;
+    public final static Integer LIMIT_CREDIT_PREMIUM = 10000;
+}
