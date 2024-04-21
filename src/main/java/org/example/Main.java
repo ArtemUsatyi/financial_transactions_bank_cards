b9c5c0ee2c305cec581d1aaec2d6e438b5f16571
artemowork <artem180791@mail.ru>
2024-04-21 22:02:37 +0300

    Главное меню запуска приложения

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.credit_card.CreditPremiumCard;
+import org.example.credit_card.CreditSimpleCard;
+import org.example.debit_card.DebitPremiumCard;
+import org.example.debit_card.DebitSimpleCard;
+import org.example.service.MenuCommandService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        CreditPremiumCard creditPremiumCard = new CreditPremiumCard();
+        CreditSimpleCard creditSimpleCard = new CreditSimpleCard();
 
+        DebitSimpleCard debitSimpleCard = new DebitSimpleCard();
+        DebitPremiumCard debitPremiumCard = new DebitPremiumCard();
+
+        List<BankCard> cardList = new ArrayList<>();
+        cardList.add(debitSimpleCard);
+        cardList.add(debitPremiumCard);
+        cardList.add(creditSimpleCard);
+        cardList.add(creditPremiumCard);
+
+        MenuCommandService.printMenuCommand(cardList);
     }
 }
